051610c50a1562d1d6b2767cbf05374c0ec1fd91
panickat <krizkrag@gmail.com>
2020-02-20 01:11:11 -0600

    error 404 main,templates

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 todos = ['Lupita Gallardo', 'Gerardo prensa', 'Lic. Alverto']
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
