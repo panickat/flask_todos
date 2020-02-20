6357ab18482dd5d70380f7b7bac8e559c66e037b
panickat <krizkrag@gmail.com>
2020-02-20 13:11:10 -0600

    vid 16. use of session object from flask instead of cookies

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+#app.config['ENV'] = 'development' #not work
+app.config['SECRET_KEY'] = 'super secretow'
 bootstrap = Bootstrap(app)
 
 todos = ['Lupita Gallardo', 'Gerardo prensa', 'Lic. Alverto']
@@ -15,13 +17,13 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip',user_ip)
+    session['user_ip'] = user_ip #response.set_cookie('user_ip',user_ip)
     return response
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')#request.cookies.get('user_ip')
     context = {'user_ip': user_ip, 'todos': todos}
     return render_template('hello.html',**context)
 #export FLASK_APP=main.py
-#export FLASK_DEBUG=1
+#export FLASK_ENV=development ##export FLASK_DEBUG=1
