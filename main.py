97144e99f74a2d21057eb588d85767a3fa3c4f8a
panickat <krizkrag@gmail.com>
2020-02-21 14:59:53 -0600

    vid 22 app factory, it works well

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField,SubmitField
-from wtforms.validators import DataRequired
+from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
+from app import create_app
+from app.forms import LoginForm
 
-app = Flask(__name__)
-#app.config['ENV'] = 'development' #not work
-app.config['SECRET_KEY'] = 'super secretow'
-bootstrap = Bootstrap(app)
-
+app = create_app()
 todos = ['Lupita Gallardo', 'Gerardo prensa', 'Lic. Alverto']
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
 
 @app.cli.command()
 def test():
@@ -54,6 +43,5 @@ def hello():
         return redirect(url_for('index'))
 
     return render_template('hello.html',**context)
-#export FLASK_APP=main.py
-#export FLASK_ENV=development ##export FLASK_DEBUG=1
+#export FLASK_APP=main.py && export FLASK_ENV=development && export FLASK_DEBUG=1
 
