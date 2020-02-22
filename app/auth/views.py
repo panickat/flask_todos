edb33fc0dfb40ff3bea1e9aa0a6d300d376d6ed9
panickat <krizkrag@gmail.com>
2020-02-21 20:44:56 -0600

    vid 30. user authentication 2: until minute 7:00

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,9 @@
 from flask import render_template, session, redirect, flash, url_for
+from flask_login import login_user
 from app.forms import LoginForm
 from . import auth
+from app.firestore_service import get_user
+from app.models import UserModel, UserData
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -11,9 +14,23 @@ def login():
 
     if login_form.validate_on_submit():
         username = login_form.username.data
-        session['username'] = username
+        password = login_form.password.data
+        user_doc = get_user(username)
 
-        flash('User name successfully registered')
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+
+            if password == password_from_db:
+                user_data = UserData(username,password)
+                user = UserModel(user_data)
+
+                login_user(user)
+                flash('Well come again')
+                redirect(url_for('hello'))
+            else:
+                flash('wrong password >:(')
+        else:
+            flash('user does not exist =/')
         return redirect(url_for('index'))
     
     return render_template('login.html', **context)
