4f4e3d7d1959783fcf8e2ff0688ca55fde21ac60
panickat <krizkrag@gmail.com>
2020-02-23 22:10:27 -0600

    vid 34. Edit tasks, update_todo

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import request, make_response, redirect, render_template, session, ur
 from flask_login import login_required, current_user
 
 from app import create_app
-from app.forms import TodoForm, DeleteTodoForm
-from app.firestore_service import get_users, get_todos, put_todo, delete_todo
+from app.forms import TodoForm, DeleteTodoForm, UpdateTodoForm
+from app.firestore_service import update_todo, get_todos, put_todo, delete_todo, update_todo
 
 app = create_app()
 
@@ -32,13 +32,15 @@ def hello():
     username = current_user.id
     todo_form = TodoForm()
     delete_form = DeleteTodoForm()
+    update_form = UpdateTodoForm()
 
     context = {
         'user_ip': user_ip,
         'todos': get_todos(user_id=username),
         'username': username,
         'todo_form': todo_form,
-        'delete_form': delete_form
+        'delete_form': delete_form,
+        'update_form': update_form 
         }
 
     if todo_form.validate_on_submit():
@@ -56,7 +58,12 @@ def delete(todo_id):
 
     return redirect(url_for('hello'))
 
+@app.route('/todos/update/<todo_id>/<int:done>', methods=['POST'])
+def update(todo_id, done):
+    user_id = current_user.id
+
+    update_todo(user_id=user_id, todo_id=todo_id, done=done)
+    return redirect(url_for('hello'))
 
 #export FLASK_APP=main.py && export FLASK_ENV=development && export FLASK_DEBUG=1
-
-
+print(":S")
