967b07065d0985d03ba496d965c2e75d72779d16
panickat <krizkrag@gmail.com>
2020-02-23 18:58:49 -0600

    vid 31. Singup encrypted

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField,SubmitField
+from wtforms.fields import StringField, PasswordField,SubmitField,HiddenField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+    
