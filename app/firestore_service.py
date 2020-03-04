a5035543c033d337ac07618f21d27b8cfdbddfc6
panickat <krizkrag@gmail.com>
2020-03-04 09:56:02 -0600

    rating +,- ready, forthcoming search user

--- app/firestore_service.py
+++ app/firestore_service.py
@@ -19,26 +19,21 @@ def user_put(user_data):
     user_ref = db.collection('users').document(user_data.username)
     user_ref.set({'password': user_data.password })
 
-# def get_daily(user_id):
-#     ref = db.collection('users').document(user_id).collection('profile').document(today).get()
-#     if ref 
-#         return ref
-#     else:
-#         pass
-
-
-def _get_profile(user_torate):
-    return db.document('users/{}/profile/{}'.format(user_torate,today)) # db.collection('users').document(user_torate).collection('profile').document(today)
+def profile(user_id):
+    return db.document('users/{}/profile/{}'.format(user_id,today)) # db.collection('users').document(user_torate).collection('profile').document(today)
     
-# ipc
-def put_daily_rate(user_id, user_torate, spent_over, spent_under, well, poor):
-    exist = db.document('users/{}/profile/{}'.format(user_torate,today)).get()
-    profile = _get_profile(user_torate)
-    if exist.to_dict() is None:
-        profile.set({ 'spent_over': False, 'spent_under': False, 'well':False, 'poor':False })
+def get_daily(user_id):
+    ref = profile(user_id)
+    _get = ref.get()
+    if _get.exists:
+        return _get
     else:
-        spent_over = not bool(spent_over)
-        profile.update({ 'spent_over': spent_over, 'spent_under': spent_under, 'well':well, 'poor':poor })
-    return db.document('users/{}/profile/{}'.format(user_torate,today)).get()
+        ref.set({ 'spent_over': False, 'spent_under': False})
+        return ref.get()
 
+def update_qualify(user_id, user_torate, event):
+    ref = profile(user_id)
+    ref.update({ 'spent_'+event: True })
 
+    ref = profile(user_torate)
+    ref.update({ 'point_'+event: 1 })
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask_login import login_required, current_user
 from app import create_app
 
 from app.forms import DeletedailyForm, UpdatedailyForm, SpentOverForm, SpentUnderForm, dailyForm
-from app.firestore_service import put_daily_rate
+from app.firestore_service import get_daily, update_qualify
 
 app = create_app()
 
@@ -35,7 +35,6 @@ def hello():
     spentover_form = SpentOverForm()
     spentunder_form = SpentUnderForm()
 
-    username = current_user.id
     daily_form = dailyForm()
     delete_form = DeletedailyForm()
     update_form = UpdatedailyForm()
@@ -45,44 +44,22 @@ def hello():
 
         'spentover_form': spentover_form,
         'spentunder_form': spentunder_form,        
-        'daily': put_daily_rate(user_id=username, user_torate=user_torate ,spent_over=False, spent_under=False,
-        well=1, poor=1),
-        'username': username,
+        'daily': get_daily(user_id=current_user.id),
         'daily_form': daily_form,
         'delete_form': delete_form,
         'update_form': update_form 
         }
 
-    if daily_form.validate_on_submit():
-        put_daily_rate(user_id=username, user_torate=user_torate ,spent_over=False, spent_under=False,
-        well=1, poor=1) #description=daily_form.description.data)
+    if daily_form.validate_on_submit(): # change name to usert_torate
+        #description=daily_form.description.data)
         flash('El reporte se creo con exito')
-
         return redirect(url_for('hello'))           
 
     return render_template('hello.html',**context)
 
-# @app.route('/daily/delete/<daily_id>', methods=['POST'])
-# def delete(daily_id):
-#     user_id = current_user.id
-#     delete_daily(user_id=user_id, daily_id=daily_id)
-
-#     return redirect(url_for('hello'))
-
-# @app.route('/daily/update/<daily_id>/<int:done>', methods=['POST'])
-# def update(daily_id, done):
-#     user_id = current_user.id
-
-#     update_daily(user_id=user_id, daily_id=daily_id, done=done)
-#     return redirect(url_for('hello'))
-
-@app.route('/daily/update_qualify/<daily_id>/<user_torate>/<int:spent_over>/', methods=['POST'])
-def update_qualify(daily_id, user_torate, spent_over):
-    print(daily_id,"\n########antes#########\n",user_torate,spent_over)
-    print("\n########despues#########\n",spent_over)
-    if not spent_over:
-        put_daily_rate(user_id=0,user_torate=user_torate,spent_over=spent_over,spent_under=False,well=1,poor=1)
-
+@app.route('/daily/update_qualify/<user_torate>/<event>', methods=['POST'])
+def qualify(user_torate,event):
+    update_qualify(user_id=current_user.id, user_torate=user_torate, event=event)
     return redirect(url_for('hello'))
 
 #export FLASK_APP=main.py && export FLASK_ENV=development && export FLASK_DEBUG=1 && export GOOGLE_APPLICATION_CREDENTIALS=/Users/panic/Documents/_pk/milieu.json
