66bf3de0982d43c60cb1eb3e6aef64d109a26e68
panickat <krizkrag@gmail.com>
2020-03-04 21:09:59 -0600

    filter started

---
+++ app/static/js/main.js
@@ -0,0 +1,18 @@
+
+jQuery(function($){
+
+    $(document).ready(function(){
+      $("#listSearch").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myList li").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+    });    
+    
+    console.log("inside")
+ });
+
+ console.log("outside")
+
+    
